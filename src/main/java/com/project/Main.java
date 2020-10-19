60c37b0c0e5fba05cd231021a7e8b168dc7f3ad6
hellosandile <mkhizenathan@gmail.com>
2020-10-19 17:19:45 +0200

    not done lol

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -2,7 +2,21 @@ package com.project;
 
 public class Main {
 
-    public static void main(String[] args) {
-        GameLogic.startGame();
+    public static void main(String[] args)
+    {
+        if (args.length == 1)
+        {
+            if (args[0].equalsIgnoreCase("console"))
+            {
+                GameLogic.startGame();
+            } else
+            {
+                System.out.println("type \"console\" as an argument.");
+            }
+        } else
+        {
+            System.out.println("type \"console\" as an argument.");
+        }
+        
     }
 }
